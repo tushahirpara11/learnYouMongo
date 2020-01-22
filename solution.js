8d12fdeef1223fbc11315b551df8ed59394c777d
tusharhirpara11 <tusharhirpara98@gmail.com>
2020-01-22 22:53:17 +0530

    update operation done

--- solution.js
+++ solution.js
@@ -1,7 +1,8 @@
-var mongo = require('mongodb').MongoClient
+const mongo = require('mongodb').MongoClient
 
 const process = require('process');
 const age = parseInt(process.argv[2]);
+
 let firstName = process.argv[2];
 let lastName = process.argv[3];
 let data = {
@@ -12,18 +13,22 @@ let data = {
 let url = "mongodb://localhost:27017/learnyoumongo";
 mongo.connect(url, function (err, db) {
   let dbo = db.db("learnyoumongo");
-  /*    (dbo.collection("parrots").find({age:{$gt:+age}})).toArray((err, res) => {
-        if (err) throw err;
-        console.log(res);
-      });
-      (dbo.collection("parrots").find({age:{$gt:+age}},{projection:{name:1,age:1,_id:0}})).toArray((err, res) => {
-        if (err) throw err;
-        console.log(res);
-        dbo.close();
-      })*/
+  (dbo.collection("parrots").find({ age: { $gt: +age } })).toArray((err, res) => {
+    if (err) throw err;
+    console.log(res);
+  });
+  (dbo.collection("parrots").find({ age: { $gt: +age } }, { projection: { name: 1, age: 1, _id: 0 } })).toArray((err, res) => {
+    if (err) throw err;
+    console.log(res);
+    dbo.close();
+  })
   dbo.collection('users').insert(data, function (err) {
     if (err) throw err;
     console.log(JSON.stringify(data));
     dbo.close();
   });
+  (dbo.collection('users').update({ username: 'tina' }, { $set: { age: 32 } }).toArray((err) => {
+    if (err) throw err;
+    dbo.close();
+  }));
 })
