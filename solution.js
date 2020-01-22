1353a5f312f75a8e230ee7f1da7404c01cbc22d5
tusharhirpara11 <tusharhirpara98@gmail.com>
2020-01-23 02:08:24 +0530

    count function completed

--- solution.js
+++ solution.js
@@ -13,34 +13,41 @@ let data = {
 let url = "mongodb://localhost:27017/learnyoumongo";
 // let url = "mongodb://localhost:27017/" + process.argv[2];
 mongo.connect(url, function (err, db) {
-  let dbo = db.db(learnyoumongo);
+  let dbo = db.db('learnyoumongo');
   // let dbo = db.db(process.argv[2]);
-  //Find Function
-  (dbo.collection("parrots").find({ age: { $gt: +age } })).toArray((err, res) => {
-    if (err) throw err;
-    console.log(res);
-  });
-  //Find with Projection
-  (dbo.collection("parrots").find({ age: { $gt: +age } }, { projection: { name: 1, age: 1, _id: 0 } })).toArray((err, res) => {
-    if (err) throw err;
-    console.log(res);
-    dbo.close();
-  });
-  //Insert Function
-  dbo.collection('users').insert(data, function (err) {
-    if (err) throw err;
-    console.log(JSON.stringify(data));
-    dbo.close();
-  });
-  //Update Function
-  (dbo.collection('users').update({ username: 'tina' }, { $set: { age: 32 } }).toArray((err) => {
-    if (err) throw err;
-    dbo.close();
-  }));
+  // //Find Function
+  // (dbo.collection("parrots").find({ age: { $gt: +age } })).toArray((err, res) => {
+  //   if (err) throw err;
+  //   console.log(res);
+  // });
+  // //Find with Projection
+  // (dbo.collection("parrots").find({ age: { $gt: +age } }, { projection: { name: 1, age: 1, _id: 0 } })).toArray((err, res) => {
+  //   if (err) throw err;
+  //   console.log(res);
+  //   dbo.close();
+  // });
+  // //Insert Function
+  // dbo.collection('users').insert(data, function (err) {
+  //   if (err) throw err;
+  //   console.log(JSON.stringify(data));
+  //   dbo.close();
+  // });
+  // //Update Function
+  // (dbo.collection('users').update({ username: 'tina' }, { $set: { age: 32 } }).toArray((err) => {
+  //   if (err) throw err;
+  //   dbo.close();
+  // }));
   //Remove Function
+  // if (err) throw err;
+  // (dbo.collection(process.argv[3]).remove({ _id: process.argv[4] }).toArray((err) => {
+  //   if (err) throw err;
+  //   dbo.close();
+  // }));
+  //Count Function
   if (err) throw err;
-  (dbo.collection(process.argv[3]).remove({ _id: process.argv[4] }).toArray((err) => {
+  dbo.collection('parrots').count({ age: { $gt: +age } }, function (err, count) {
     if (err) throw err;
+    console.log(count)
     dbo.close();
-  }));
+  });
 });
